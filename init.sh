d9d964b3b0581f52e6f1af2be194e56990a613c6
enth <chabanenko.sergey@gmail.com>
2016-03-06 11:55:59 +0500

    gotovo4

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+mkdir /home/box/web/uploads
+sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo cp /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
