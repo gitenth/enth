75293dd57f6169f8924e998d77fe3bada1be02d2
enth <chabanenko.sergey@gmail.com>
2016-03-06 10:18:14 +0500

    gotovo1

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
