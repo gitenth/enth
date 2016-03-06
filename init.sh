399c60e5ecc4ae0c7537fe29646282406f1ff824
enth <chabanenko.sergey@gmail.com>
2016-03-06 14:30:02 +0500

    gotovo!

--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ sudo /etc/init.d/nginx restart
 #там два каталога, sites-available, и sites-enabled, заходите в sites-available,
 #копируете default в какой-то новый свой, там все удаляете и пишете что нужно,
 #делаете символическую ссылку этого своего конфига в sites-enabled, удаляете из sites-enabled default,
-#перезапускаете сервис nginx
+#перезапускаете сервис
