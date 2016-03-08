318fe1599913c33de0af8411fd8c301aab47b4c9
enth <chabanenko.sergey@gmail.com>
2016-03-08 17:59:39 +0500

    123

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+cat /etc/gunicorn.d/wsgi.example
+
 
 #там два каталога, sites-available, и sites-enabled, заходите в sites-available,
 #копируете default в какой-то новый свой, там все удаляете и пишете что нужно,
