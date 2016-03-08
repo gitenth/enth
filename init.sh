0bbee3be3593915863fbebb1245a676b48d5fbf6
enth <chabanenko.sergey@gmail.com>
2016-03-08 17:29:24 +0500

    123

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-git clone https://github.com/gitenth/enth web
 mkdir /home/box/web/uploads
 sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 
 #там два каталога, sites-available, и sites-enabled, заходите в sites-available,
 #копируете default в какой-то новый свой, там все удаляете и пишете что нужно,
