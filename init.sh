308a66a8bba4cc367e5f5dc3c79bdf167b8397d7
enth <chabanenko.sergey@gmail.com>
2016-03-12 09:57:19 +0500

    ask2

--- init.sh
+++ init.sh
@@ -5,7 +5,17 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-cat /etc/gunicorn.d/wsgi.example
+#sudo gunicorn -b 0.0.0.0:8080 hello:application
+cd /home/box/web
+django-admin startproject ask
+cd /home/box/web/ask
+python manage.py startapp qa
+rm /home/box/web/ask/qa views.py
+cp /home/box/web/views.py /home/box/web/ask/qa/
+rm /home/box/web/ask/ask/urls.py
+cp /home/box/web/urls.py /home/box/web/ask/ask/
+cd /home/box/web/ask/ask
+sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
 
 
 #там два каталога, sites-available, и sites-enabled, заходите в sites-available,
