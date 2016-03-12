ec1d60319364c37a150d4737fc14510650ad4719
enth <chabanenko.sergey@gmail.com>
2016-03-12 10:13:43 +0500

    ask2

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from ask import views
+from qa import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,12 +7,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'$', views.test),
-    url(r'login/$', views.test),
-    url(r'signup/$', views.test),
-    url(r'ask/$', views.test),
-    url(r'popular/$', views.test),
-    url(r'new/$', views.test),
-    url(r'questions/\d+/$', views.test),
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^ask/', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
+    url(r'^questions/\d+/$', views.test),
     url(r'^admin/', include(admin.site.urls)),
 )
