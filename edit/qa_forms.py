9ddbf2c60ae2df7b2bc281b8f98c37589a08b36c
enth <chabanenko.sergey@gmail.com>
2016-03-20 19:03:38 +0500

    2.7

--- edit/qa_forms.py
+++ edit/qa_forms.py
@@ -3,11 +3,18 @@ from django.forms import ModelForm
 from qa.models import Question, Answer
 
 class AskForm(ModelForm):
+    def save(self, author, commit=True):
+        # Don't commit the results yet
+        news = AskForm.save(self, commit=False)
+        news.author = author
+        if commit:
+            news.save()
+        return news
     class Meta:
         model = Question
-        fields = ['title', 'text', 'author']
+        fields = ['title', 'text']
 
 class AnswerForm(ModelForm):
     class Meta:
         model = Answer
-        fields = ['text', 'question', 'author']
+        fields = ['text', 'question']
